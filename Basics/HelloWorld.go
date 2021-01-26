558ea0ac7c33fa8b59303f7a228585c45c1566eb
GuND0Wn151 <58622550+GuND0Wn151@users.noreply.github.com>
2021-01-26 19:54:51 +0530

    First golang code

---
+++ Basics/HelloWorld.go
@@ -0,0 +1,19 @@
+
+//this is the package for demo
+package main
+
+import "fmt"
+//this is the package fmt which contains all the funstions like print input etc
+//this package fmt is similar to the stdio.h in c
+
+func main() {
+	fmt.Println("Hello, There")//printing to the console
+}
+
+
+
+//when we have to run the file 
+//type go run filename.go
+
+//we can also create a exe file with go which is extremly good
+//go build filename.go
